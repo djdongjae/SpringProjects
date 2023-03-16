9960bd296e7a2fbcc41c09883080632cf43f71a5
Dong-Jae Oh <isf1999@naver.com>
2023-03-16 11:17:53 +0900

    no message

---
+++ param_2nd/src/main/java/net/skhu/Param2ndApplication.java
@@ -0,0 +1,13 @@
+package net.skhu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Param2ndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Param2ndApplication.class, args);
+	}
+
+}
