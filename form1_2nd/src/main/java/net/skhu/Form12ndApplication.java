afcc4a9945614564aa513d4bb12766264bd154f2
djdongjae <isf1999@naver.com>
2023-04-07 13:52:08 +0900

    2-02 spring form1 연습

---
+++ form1_2nd/src/main/java/net/skhu/Form12ndApplication.java
@@ -0,0 +1,13 @@
+package net.skhu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Form12ndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Form12ndApplication.class, args);
+	}
+
+}
---
+++ form1_2nd/src/main/java/net/skhu/controller/StudentController.java
@@ -0,0 +1,60 @@
+package net.skhu.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import net.skhu.dto.Student;
+import net.skhu.mapper.StudentMapper;
+
+@Controller
+@RequestMapping("student")
+public class StudentController {
+
+	@Autowired StudentMapper studentMapper;
+	
+	@RequestMapping("list")
+	public String list(Model model) {
+		List<Student> students = studentMapper.findAll();
+		model.addAttribute("students", students);
+		return "student/list";
+	}
+	
+	@GetMapping("create")
+	public String create(Model model) {
+		model.addAttribute("student", new Student());
+		return "student/edit";
+	}
+	
+	@PostMapping("create")
+	public String create(Model model, Student student) {
+		studentMapper.insert(student);
+		model.addAttribute("message", "저장했습니다");
+		return "student/edit";
+	}
+	
+	@GetMapping("edit")
+	public String edit(Model model, int id) {
+		Student student = studentMapper.findOne(id);
+		model.addAttribute("student", student);
+		return "student/edit";
+	}
+	
+	@PostMapping("edit")
+	public String edit(Model model, Student student) {
+		studentMapper.update(student);
+		model.addAttribute("message", "저장했습니다.");
+		return "student/edit";
+	}
+	
+	@RequestMapping("delete")
+	public String delete(Model model, int id) {
+		studentMapper.delete(id);
+		return "redirect:list";
+	}
+}
