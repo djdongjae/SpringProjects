7a54ee7040fc0ba20ef58fc313257c03f24533d9
djdongjae <isf1999@naver.com>
2023-04-10 16:35:32 +0900

    no message

---
+++ request1_1st/src/main/java/net/skhu/Request11stApplication.java
@@ -0,0 +1,13 @@
+package net.skhu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Request11stApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Request11stApplication.class, args);
+	}
+
+}
