91e4a0691a3d6dadcb6be3370af6e10ef500aa34
djdongjae <isf1999@naver.com>
2023-04-15 20:19:30 +0900

    no message

--- mybatis2_1st/src/main/java/net/skhu/dto/Department.java
+++ mybatis2_1st/src/main/java/net/skhu/dto/Department.java
@@ -1,9 +1,12 @@
 package net.skhu.dto;
 
+import java.io.Serializable;
+
 import lombok.Data;
 
 @Data
-public class Department {
+public class Department implements Serializable {
+	private static final long serialVersionUID = 1L;
 	
 	int id;
 	String name;
--- mybatis2_1st/src/main/java/net/skhu/mapper/DepartmentMapper.java
+++ mybatis2_1st/src/main/java/net/skhu/mapper/DepartmentMapper.java
@@ -2,12 +2,14 @@ package net.skhu.mapper;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.CacheNamespace;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
 import net.skhu.dto.Department;
 
 @Mapper
+@CacheNamespace(flushInterval=10000)
 public interface DepartmentMapper {
 	
 	@Select(""" 
