990ed64c9028e945267c5ec05ba4a5efdbd1a18b
djdongjae <isf1999@naver.com>
2023-04-26 17:31:15 +0900

    mybatis_resultmap

---
+++ order1_1st/src/main/java/net/skhu/Order11stApplication.java
@@ -0,0 +1,13 @@
+package net.skhu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Order11stApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Order11stApplication.class, args);
+	}
+
+}
