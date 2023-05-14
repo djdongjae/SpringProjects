0b15f722c99d2699e23bf0aca9a72f60646a09f3
djdongjae <isf1999@naver.com>
2023-05-14 17:20:15 +0900

    jpa single-valued association 연습

---
+++ book2_2023-05-14-1545i/src/main/java/net/skhu/Book2202305141545iApplication.java
@@ -0,0 +1,13 @@
+package net.skhu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Book2202305141545iApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Book2202305141545iApplication.class, args);
+	}
+
+}
