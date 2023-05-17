af2aac1c0213c2138da27a0bca11d01d8ca660f2
djdongjae <isf1999@naver.com>
2023-05-17 16:49:55 +0900

    7-03 jpa collection-valued association

---
+++ jpa3_2023-05-17-1529i/src/main/java/net/skhu/entity/Department.java
@@ -0,0 +1,25 @@
+package net.skhu.entity;
+
+import java.util.List;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import lombok.Data;
+
+@Data
+@Entity
+public class Department {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	int id;
+	
+	String name;
+	String shortName;
+	String phone;
+	
+	@OneToMany(mappedBy="department")
+	List<Student> students;
+}
---
+++ jpa3_2023-05-17-1529i/src/test/java/net/skhu/Jpa3202305171529iApplicationTests.java
@@ -0,0 +1,13 @@
+package net.skhu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Jpa3202305171529iApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
