d851a4d24f8ad6ef667411459d785e027dbbd752
djdongjae <isf1999@naver.com>
2023-06-15 21:14:07 +0900

    no message

---
+++ t201914089/src/main/java/net/skhu/entity/Jumoon.java
@@ -0,0 +1,29 @@
+package net.skhu.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.Data;
+
+@Data
+@Entity
+public class Jumoon {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	int id;
+	
+	int quantity;
+	
+	@ManyToOne
+	@JoinColumn(name = "customerId")
+	Customer customer;
+	
+	@ManyToOne
+	@JoinColumn(name = "productId")
+	Product product;
+
+}
--- t201914089/src/main/java/net/skhu/model/Pagination.java
+++ t201914089/src/main/java/net/skhu/model/Pagination.java
@@ -5,12 +5,12 @@ import lombok.Data;
 @Data
 public class Pagination {
     int pg = 1;        // 현재 페이지
-    int sz = 15;       // 페이지 당 레코드 수
-    int di = 0;        // departmentId
+    int sz = 10;       // 페이지 당 레코드 수
+    String title = "";
     int recordCount;   // 전체 레코드 수
 
     public String getQueryString() {
-        return String.format("pg=%d&sz=%d&di=%d", pg, sz, di);
+        return String.format("pg=%d&sz=%d&title=%s", pg, sz, title);
     }
 }
 
